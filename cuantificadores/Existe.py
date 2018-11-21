4a04c749eb278b8161b2b46b60ef410e2c6dd84f
Roberto Suárez <electrosonix12@gmail.com>
2018-11-20 22:53:04 -0500

    la funcion P (existe - para todos) esta completada

--- cuantificadores/Existe.py
+++ cuantificadores/Existe.py
@@ -20,4 +20,37 @@ def ExisteAlmenosUno(D):
 
     return False
 
-print(ExisteAlmenosUno(dis))
+#print(ExisteAlmenosUno(dis))
+
+
+"""
+        existe al menos un - para todo
+        existe al menos una x en Dx tal que P(x,y) es verdadera para toda "y" en Dy     
+"""
+
+def es_divisible(x, y):
+    if(x % y == 0):
+        return True
+    return False
+
+def es_mayor(x,y):
+        if (x >= y):
+                return True
+        return False
+
+def P(dx,dy):
+        for x in dx:
+                cont = 0
+                for y in dy:
+                        if not es_divisible(x,y):
+                                break
+                        else:
+                                cont += 1
+                                if cont == len(dy):
+                                        return True
+        return False
+
+d1 = [2,3,5,16]
+d2 = [2,4]
+
+print(P(d1, d2))
