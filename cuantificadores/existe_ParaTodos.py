a7f20527891b1ed120095c99c77a95582d83aa20
Roberto <electrosonix12@gmail.com>
2018-11-28 19:35:11 -0500

    comtario
    comente todo el codigo para mejor entendimiento

--- cuantificadores/existe_ParaTodos.py
+++ cuantificadores/existe_ParaTodos.py
@@ -4,24 +4,26 @@
         existe al menos una x en Dx tal que P(x,y) es verdadera para toda "y" en Dy     
 """
 
+# func que verifica se x es divible para y.
 def es_divisible(x, y):
     if(x % y == 0):
         return True
     return False
 
 def P(dx,dy):
-        for x in dx:
+        for x in dx: # listamo el discurso dx
                 cont = 0
-                for y in dy:
-                        if not es_divisible(x,y):
+                for y in dy:	# listamo el discruso dy
+                        if not es_divisible(x,y):	# si no es divisble entonces que se salte esta iteración
                                 break
-                        else:
-                                cont += 1
-                                if cont == len(dy):
+                        else:	# si es dibisible entoces 
+                                cont += 1	# agregamos 1 al contador
+                                if cont == len(dy):	# y si todos los elementos de dy son divisibles entoces retorna verdadero
+                                        print("{} es dibisible para {}".format(x, dy))
                                         return True
         return False
 
-d1 = [2,3,5,16]
-d2 = [2,4]
+d1 = [2,3,5,16]		# Discurso uno 
+d2 = [2,4]			# discruso dos
 
 print(P(d1, d2))
